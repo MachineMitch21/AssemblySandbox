f6ee8bc91aecbf05b61ede373148f9cb35ee4776
MachineMitch21 <mitchschutt@gmail.com>
2019-06-28 08:24:29 -0400

    Added missing defs in str_utils.h

--- src/str_utils.h
+++ src/str_utils.h
@@ -1,6 +1,10 @@
 #ifndef STR_UTILS_H
 #define STR_UTILS_H
 
-
+void strrev(char* str);
+void to_string(char* buffer, int n, int divisor);
+char to_lower(char in);
+char to_upper(char in);
+unsigned int is_palindrome(const char* str);
 
 #endif 
